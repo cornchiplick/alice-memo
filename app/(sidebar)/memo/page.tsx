f2e2cf8d1fc4cb6b9e9077a79236baf65e296e00
cornchiplick <ilsunkiss@gmail.com>
2024-12-16 17:19:52 +0900

    Feat: client 측 쿠키를 이용한 메모 저장 로직 일부 개발 - 해당 로직은 비로그인 상태인 경우를 상정했다. - 메모를 작성하면 client 측 쿠키에 저장됨 - 해당 쿠키들의 key를 저장하는 쿠키를 저장한다. - 화면 조회시 모든 쿠키정보가 있는 쿠키를 토대로 각 쿠키정보들을 렌더링함

--- app/(sidebar)/memo/page.tsx
+++ app/(sidebar)/memo/page.tsx
@@ -1,5 +1,68 @@
+"use client";
+
+import {COOKIE} from "@/constants/constants";
+import {getCookie, setCookie} from "cookies-next";
+import crypto from "crypto";
+import {useEffect, useState} from "react";
+
 const MemoHome = () => {
-  return <h1>Memo!</h1>;
+  const [allMemo, setAllMemo] = useState<string[]>([]);
+
+  useEffect(() => {
+    const memoAll = getCookie(COOKIE.ALICE_MEMO_ALL);
+    let memoArray = [];
+    if (!!memoAll) {
+      memoArray = JSON.parse(memoAll as string);
+      setAllMemo(memoArray);
+    }
+  }, []);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const content = formData.get("content");
+
+    const uuid = crypto.randomBytes(16).toString("hex");
+    const itemKey = `${COOKIE.ALICE_MEMO_ITEM}_${uuid}`;
+
+    setCookie(itemKey, content);
+
+    const memoAll = getCookie(COOKIE.ALICE_MEMO_ALL);
+
+    let memoArray = [];
+    if (memoAll) {
+      try {
+        memoArray = JSON.parse(memoAll as string);
+      } catch (e) {
+        console.error("Failed to parse ALICE_MEMO_ALL cookies: ", e);
+      }
+    }
+
+    memoArray.push(itemKey);
+    setCookie(COOKIE.ALICE_MEMO_ALL, JSON.stringify(memoArray));
+    setAllMemo(memoArray);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          required
+          type="text"
+          placeholder="content"
+          name="content"
+          className="rounded-md border-none bg-transparent"
+        />
+        <button type="submit">추가</button>
+      </form>
+      <div>
+        <h2>All Memos</h2>
+        {allMemo.map((memo) => (
+          <div key={memo}>{memo}</div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MemoHome;
