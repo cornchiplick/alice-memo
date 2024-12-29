2cd489271ee0cd1ce48a8496037f0b3cf0d0ebac
cornchiplick <ilsunkiss@gmail.com>
2024-12-29 22:32:20 +0900

    Fix: 시간 순서로 정렬이 가능하게 uuid 생성 로직 일부 수정

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,6 +1,6 @@
 export const generateUUID = (prefix: string = "") => {
   let date = Date.now();
-  const uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+  const uuid = "xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx".replace(/[xy]/g, function (c) {
     const r = (date + Math.random() * 16) % 16 | 0;
     date = Math.floor(date / 16);
     return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
